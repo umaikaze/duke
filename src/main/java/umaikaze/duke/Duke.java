969a8af4570eddbf46a8e6f899819268eebab345
umaikaze <zmj10086@gmail.com>
2020-02-04 22:55:16 +0800

    Add UI elements to anchor pane

--- src/main/java/umaikaze/duke/Duke.java
+++ src/main/java/umaikaze/duke/Duke.java
@@ -12,7 +12,13 @@ import java.time.format.DateTimeParseException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -20,6 +26,12 @@ public class Duke extends Application{
     Ui ui;
     Storage st;
     TaskList tl;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     public Duke() throws UnsupportedEncodingException {
         ui = new Ui();
@@ -98,7 +110,55 @@ public class Duke extends Application{
         helloWorld.setFont(new Font("Comic Sans MS", 22));
         Scene scene = new Scene(helloWorld);
 
+        setUiElements();
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
         stage.setScene(scene);
         stage.show();
+
+        setUiFormatting(stage, mainLayout);
+    }
+
+    private void setUiElements() {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+    }
+
+    private void setUiFormatting(Stage stage, AnchorPane mainLayout) {
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
